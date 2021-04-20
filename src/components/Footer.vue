46d48e9623b0a798b2d8c4c3a1feaba31ef40354
dara <vuthydaraa@gmail.com>
2021-04-20 22:38:07 +0700

    Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="footer">
+    <div>
+      <p>Center & Clinic</p>
+      <hr>
+      <a href="#" class="func">Make an Appointment</a>
+      <a href="#" class="func">Patient's Story</a>
+    </div>
+    <div>
+      <p>Health Tips</p>
+      <hr>
+      <a href="#" class="func">Health Tips</a>
+      <a href="#" class="func">Health Tips Video</a>
+      <a href="#" class="func">Disease & Treatment</a>
+    </div>
+    <div>
+      <p>Contact Us</p>
+      <hr>
+      <a href="#" class="func">Contact Us</a>
+      <a href="#" class="func">Customer Feedback</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
